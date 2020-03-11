5d654d77323b4f4b14b42ca0e95401df313db3e1
dgs4349 <dgs4349@rit.edu>
2020-03-11 16:46:49 -0400

    fixing dropdown

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 
-import '../Assets/css/fonts.css';
-
-
 import { makeStyles } from "@material-ui/core/styles";
+import { NativeSelect, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
+
     container: {
         textAlign: 'center',
         position: 'relative',
@@ -13,21 +12,50 @@ const useStyles = makeStyles({
     headerWrapper: {
         color: "#2f2f2f",
     },
+
     title: {
-        fontWeight: 900,
-        fontFamily: 'Playfair Display, serif',
-        fontSize: '80px',
-        textTransform: 'uppercase',
+        fontFamily: 'Olde-English, Playfair Display, serif',
+        fontSize: '96px',
         lineHeight: "72px",
-        marginBottom: 20,
+        margin: '15px 0 20px 0',
     },
     subtitle: {
         fontFamily: 'Lora',
+        fontSize: '14px',
+        fontWeight: '100',
         textTransform: 'uppercase',
-        borderBottom: '2px solid #2f2f2f',
-        borderTop: '2px solid #2f2f2f',
-        padding: '12px 0 12px 0',
+        borderBottom: '1px solid #2f2f2f',
+        borderTop: '1px solid #2f2f2f',
+        padding: '6px 0 6px 0',
     },
+
+    sectionSelect: {
+        fontFamily: 'Lora',
+        fontWeight: '100',
+        position: 'relative',
+        left: '0px',
+        textAlign: 'left',
+        marginLeft: 20,
+        fontSize: '14px',
+        paddingRight: 0,
+    },
+    sectionItem: {
+        display: 'inline-block',
+        fontSize: '14px',
+
+        fontFamily: 'Lora',
+        fontWeight: '100',
+        position: 'relative',
+        left: '0px',
+        textAlign: 'left',
+        marginLeft: 20,
+        paddingRight: 0,
+    },
+
+    date: {
+        display: 'inline-block',
+        textAlign: 'center',
+    }
 });
 
 
@@ -36,7 +64,11 @@ export default function Header(props){
     const classes = useStyles();
     const { ...rest } = props;
 
+    const event = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
     const titleId = 'fontTitleUpdateTarget';
+    
 
     useEffect(() => {
         // let title = document.getElementById(titleId);
@@ -48,12 +80,43 @@ export default function Header(props){
         <div className={classes.container}>
             <div className={classes.headerWrapper}>
                 <div className={classes.title}>
-                    The Newsport Times
+                    The Davis Smith Report
                 </div>   
 
-                <div className={classes.subtitle}>
-                    Is a Game and Audio Developer
-                </div>
+                <Grid container className={classes.subtitle}>
+
+                    <Grid item xs={4} style={{textAlign: "left",}}>
+                        <NativeSelect 
+                        className={classes.sectionSelect} 
+                        defaultValue={0}
+                        style={{padding: 0}}
+                        >
+                            <option className={classes.sectionItem} value={0}>
+                                Today's Featured News
+                                </option>
+                            <option className={classes.sectionItem} value={1}>
+                                Audio Projects
+                                </option>
+                            <option className={classes.sectionItem} value={2}>
+                                Game Projects
+                                </option>
+                            <option className={classes.sectionItem} value={3}>
+                                Web Projects
+                                </option>
+                            <option className={classes.sectionItem} value={4}>
+                                Graphics and Other Projects
+                                </option>
+                        </NativeSelect>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <div className={classes.date}>
+                            {event.toLocaleDateString('en-US', options)}
+                        </div>
+                    </Grid>
+
+                    <Grid item xs={4}></Grid>
+                </Grid>
             </div>
         </div>
         );
