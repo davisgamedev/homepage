a1920e6242c158473458189a42dea8990325bc2c
dgs4349 <dgs4349@rit.edu>
2020-03-27 23:16:53 -0400

    cleanup, and attempt to publish to docs folder

---
+++ docs/precache-manifest.67c1cbd35e25fb1b1921ee538a77f1a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19257e253655adc4db4edbc98d26de95",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "8305b8cc4d9df2af4031",
+    "url": "/website/static/css/main.d4ddddcd.chunk.css"
+  },
+  {
+    "revision": "02774729915aa4a7f231",
+    "url": "/website/static/js/2.4b5b495b.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "/website/static/js/2.4b5b495b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8305b8cc4d9df2af4031",
+    "url": "/website/static/js/main.c42dfa1c.chunk.js"
+  },
+  {
+    "revision": "bd62843c225112ca44b6",
+    "url": "/website/static/js/runtime-main.1a6b2526.js"
+  },
+  {
+    "revision": "713ad647718093479ef7ba690c320048",
+    "url": "/website/static/media/OldeEnglish.713ad647.ttf"
+  }
+]);
