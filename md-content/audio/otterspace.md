135599c6237adfa7202de9cba8b5a1b7c556422e
dgs4349 <dgs4349@rit.edu>
2020-03-29 18:51:20 -0400

    fixing md stuff

--- md-content/audio/otterspace.md
+++ md-content/audio/otterspace.md
@@ -1,9 +1,6 @@
 # Return to Otter Space
 ## Junior Animation and Interactive Experience
 
-Documents are still being transferred, but media links are below.
-
-<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1020777331&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
 ![](https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-ship_httiiq.png)
 ![](https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-characters_ditijq.png)
 ![](https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425859/otterspace-cave_st4skg.png)
--- src/Content-Out/audio/otterspace.jsx
+++ src/Content-Out/audio/otterspace.jsx
@@ -11,12 +11,11 @@ export default class Otterspace extends React.Component {
 <div>
 <h1 id="return-to-otter-space">Return to Otter Space</h1>
 <h2 id="junior-animation-and-interactive-experience">Junior Animation and Interactive Experience</h2>
-<p>Documents are still being transferred, but media links are below.</p>
-<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1020777331&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-![](https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-ship_httiiq.png)
-![](https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-characters_ditijq.png)
-![](https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425859/otterspace-cave_st4skg.png)
-![](https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-hub_xeqetl.png)
+<p><img src="https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-ship_httiiq.png" alt=""/>
+<img src="https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-characters_ditijq.png" alt=""/>
+<img src="https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425859/otterspace-cave_st4skg.png" alt=""/>
+<img src="https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585425860/otterspace-hub_xeqetl.png" alt=""/></p>
+
 </div>
         );
     }
