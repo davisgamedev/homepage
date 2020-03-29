f0f7ecc48b34f08d9f3859c43a6a7ab6b244172f
dgs4349 <dgs4349@rit.edu>
2020-03-29 17:00:54 -0400

    Updates

---
+++ precache-manifest.700e0360b4513920e1016be40f9a173e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68347d3145ace98ef927871f38d9851",
+    "url": "https://dgs4349.github.io/homepage/index.html"
+  },
+  {
+    "revision": "00f7459cd4b0c10629fe",
+    "url": "https://dgs4349.github.io/homepage/static/css/main.e366a0a1.chunk.css"
+  },
+  {
+    "revision": "32a40a5649083a9929ef",
+    "url": "https://dgs4349.github.io/homepage/static/js/2.8d68dab5.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "https://dgs4349.github.io/homepage/static/js/2.8d68dab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f7459cd4b0c10629fe",
+    "url": "https://dgs4349.github.io/homepage/static/js/main.82d98b05.chunk.js"
+  },
+  {
+    "revision": "72c53ce2760bc6dbedeb",
+    "url": "https://dgs4349.github.io/homepage/static/js/runtime-main.a943f763.js"
+  },
+  {
+    "revision": "713ad647718093479ef7ba690c320048",
+    "url": "https://dgs4349.github.io/homepage/static/media/OldeEnglish.713ad647.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://dgs4349.github.io/precache-manifest.c1bf2741bd093abbad28cbdbb9da648c.js"
+  "https://dgs4349.github.io/homepage/precache-manifest.700e0360b4513920e1016be40f9a173e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://dgs4349.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://dgs4349.github.io/homepage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
