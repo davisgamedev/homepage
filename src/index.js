c236ad796cd77f31e110512c1348c69a8e3ffe47
dgs4349 <dgs4349@rit.edu>
2020-03-29 16:59:52 -0400

    added redirect handler to fix routing issues

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'Assets/css/fonts.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, withRouter } from "react-router-dom";
 
 import "common/assets/scss/material-kit-react.scss?v=1.8.0";
 
@@ -16,9 +16,26 @@ import './index.css';
 
 var hist = createBrowserHistory();
 
+window.updatedPath = false;
+
+const ClearHomepageUrl = withRouter(({location, history}) => {
+    if(window.updatedPath) return null;
+
+    const paths = location.pathname.split('/').filter(x=>x); // filter removes empty strings
+
+    const newPath = paths.reduce(
+        (acc, val) => acc += (val !== "homepage") ? '/' + val : "", "");
+        
+    if(newPath !== location.pathname) history.push(newPath);
+
+    window.updatedPath = true;
+    return null;
+});
+
 ReactDOM.render(
     <Router history={hist}>
         <Breakpoints>
+            <ClearHomepageUrl />
             <Switch>
                 {/* <Route path="/examples/components" component={Components} /> */}
                 <Route path="/" component={Home}/>
