1ae32555f2a7e9f6c5e63130e87f1621b5f8765c
dgs4349 <dgs4349@rit.edu>
2020-03-29 13:07:48 -0400

    trying so hard

---
+++ docs/precache-manifest.f2ad0f19f66209fdbf88ebb83fa827b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464c972c6f8b9cf9c1a5b64ecca54d80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d85e3bddd1b8396be2b2",
+    "url": "/static/css/main.3e98cb0a.chunk.css"
+  },
+  {
+    "revision": "32a40a5649083a9929ef",
+    "url": "/static/js/2.8d68dab5.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "/static/js/2.8d68dab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85e3bddd1b8396be2b2",
+    "url": "/static/js/main.b3415349.chunk.js"
+  },
+  {
+    "revision": "63544584c900ce7838f8",
+    "url": "/static/js/runtime-main.ad331ff5.js"
+  },
+  {
+    "revision": "713ad647718093479ef7ba690c320048",
+    "url": "/static/media/OldeEnglish.713ad647.ttf"
+  }
+]);
