e204aca3c4411799e7e58de6b29521b6a8a3c61b
dgs4349 <dgs4349@rit.edu>
2020-03-29 20:00:34 -0400

    Updates

---
+++ precache-manifest.df1cda2b23378707bdfad6aa7fe77987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0cde49024383be6e8c29330487bde1",
+    "url": "https://davisgame.dev/index.html"
+  },
+  {
+    "revision": "c111d30253138aaab5ed",
+    "url": "https://davisgame.dev/static/css/main.42edc842.chunk.css"
+  },
+  {
+    "revision": "32a40a5649083a9929ef",
+    "url": "https://davisgame.dev/static/js/2.8d68dab5.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "https://davisgame.dev/static/js/2.8d68dab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c111d30253138aaab5ed",
+    "url": "https://davisgame.dev/static/js/main.141b97ff.chunk.js"
+  },
+  {
+    "revision": "74da75a49c2a6380baf0",
+    "url": "https://davisgame.dev/static/js/runtime-main.d93ed901.js"
+  },
+  {
+    "revision": "713ad647718093479ef7ba690c320048",
+    "url": "https://davisgame.dev/static/media/OldeEnglish.713ad647.ttf"
+  }
+]);
