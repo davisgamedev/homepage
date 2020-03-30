ff556392f83338201ed31520734d390427eef272
dgs4349 <dgs4349@rit.edu>
2020-03-29 21:12:29 -0400

    Updates

--- precache-manifest.865f25b43554e7a238d2d85ef0b5ebb1.js
+++ precache-manifest.865f25b43554e7a238d2d85ef0b5ebb1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14715546c260b5728a429aef98c72199",
+    "revision": "2849416278908c5401d88194444a91a7",
     "url": "https://davisgame.dev/index.html"
   },
   {
-    "revision": "f3e8917ca806fcc8ba84",
-    "url": "https://davisgame.dev/static/css/main.42edc842.chunk.css"
+    "revision": "b99cc1eb9563af81ea30",
+    "url": "https://davisgame.dev/static/css/main.37f6b5a9.chunk.css"
   },
   {
     "revision": "32a40a5649083a9929ef",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://davisgame.dev/static/js/2.8d68dab5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f3e8917ca806fcc8ba84",
-    "url": "https://davisgame.dev/static/js/main.9e0bdbd3.chunk.js"
+    "revision": "b99cc1eb9563af81ea30",
+    "url": "https://davisgame.dev/static/js/main.c6120706.chunk.js"
   },
   {
     "revision": "74da75a49c2a6380baf0",
