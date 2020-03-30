d7109967bd159723427a549ad0df685d7de47a77
dgs4349 <dgs4349@rit.edu>
2020-03-29 20:43:12 -0400

    fixing md files

--- src/Content-Out/audio/paintrain.jsx
+++ src/Content-Out/audio/paintrain.jsx
@@ -11,7 +11,6 @@ export default class Paintrain extends React.Component {
 <div>
 <h1 id="pain-train-the-final-heist">Pain Train: The Final Heist</h1>
 <h2 id="western-space-jazz-for-a-mobile-arg">Western Space Jazz For a Mobile ARG</h2>
-<p><img src="https://res.cloudinary.com/dyzmnhqpr/image/upload/v1585177799/sample.jpg" alt="test image"/></p>
 
 </div>
         );
