4a8ea96b233046f7fbf8ec3eda9eb9c03e298607
dgs4349 <dgs4349@rit.edu>
2020-03-29 20:22:29 -0400

    Updates

---
+++ precache-manifest.1075fdcce4f72a70caeb83d3385e69c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2068bcc2e2cdd2ec180d2b3fedaa4487",
+    "url": "https://dgs4349.github.io/homepage/index.html"
+  },
+  {
+    "revision": "598d577f4e66056e9b95",
+    "url": "https://dgs4349.github.io/homepage/static/css/main.39eaa060.chunk.css"
+  },
+  {
+    "revision": "32a40a5649083a9929ef",
+    "url": "https://dgs4349.github.io/homepage/static/js/2.8d68dab5.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "https://dgs4349.github.io/homepage/static/js/2.8d68dab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598d577f4e66056e9b95",
+    "url": "https://dgs4349.github.io/homepage/static/js/main.141b97ff.chunk.js"
+  },
+  {
+    "revision": "72c53ce2760bc6dbedeb",
+    "url": "https://dgs4349.github.io/homepage/static/js/runtime-main.a943f763.js"
+  },
+  {
+    "revision": "713ad647718093479ef7ba690c320048",
+    "url": "https://dgs4349.github.io/homepage/static/media/OldeEnglish.713ad647.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://davisgame.dev/precache-manifest.df1cda2b23378707bdfad6aa7fe77987.js"
+  "https://dgs4349.github.io/homepage/precache-manifest.1075fdcce4f72a70caeb83d3385e69c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://davisgame.dev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://dgs4349.github.io/homepage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
