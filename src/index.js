70c10dd5de84034a6bccbdf5fd26635e9a556546
dgs4349 <dgs4349@rit.edu>
2020-08-06 19:36:32 -0400

    webgl sandboxes

--- src/index.js
+++ src/index.js
@@ -8,33 +8,34 @@ import { Router, Route, Switch, withRouter } from "react-router-dom";
 
 import "common/assets/scss/material-kit-react.scss?v=1.8.0";
 
-import Home from "Views/Home";
-import Breakpoints from 'Tech/Breakpoints';
-import RouteUpdateHandler from 'Tech/RouteUpdateHandler';
+// import Home from "Views/Home";
+// import Breakpoints from 'Tech/Breakpoints';
+// import RouteUpdateHandler from 'Tech/RouteUpdateHandler';
 
 import './index.css';
-import Backdrop from 'Components/WebGL/Backdrop';
-import BackgroundScene from 'Components/WebGL/TestIcoBackgroundScene';
+import BackgroundFiberDemoScene from 'Components/WebGL/BackgroundFiberDemoScene';
+import IcoBackgroundScene from 'Components/WebGL/IcoBackgroundScene';
 
 var hist = createBrowserHistory();
 
 window.resetPath = false;
 
-const ResetHomePath = withRouter(({location, history}) => {
-    if(window.resetPath) return null;
+// not necessary after migration
+// const ResetHomePath = withRouter(({location, history}) => {
+//     if(window.resetPath) return null;
 
-    if(location.hash !== "") {
-        history.push(location.hash.replace('#', ''));
-    }
-    // add a check here for redirected to projects
+//     if(location.hash !== "") {
+//         history.push(location.hash.replace('#', ''));
+//     }
+//     // add a check here for redirected to projects
 
-    window.resetPath = true;
-    return null;
-});
+//     window.resetPath = true;
+//     return null;
+// });
 
 ReactDOM.render(
     <Router history={hist} basename={process.env.PUBLIC_URL}>
-       
+       {/*
         <Breakpoints>
             <RouteUpdateHandler />
             <ResetHomePath />
@@ -42,7 +43,8 @@ ReactDOM.render(
                 <Route path="" component={Home}/>
             </Switch>
         </Breakpoints>
-      
+       */}
+       <IcoBackgroundScene></IcoBackgroundScene>
     </Router>,
     document.getElementById("root")
 );
